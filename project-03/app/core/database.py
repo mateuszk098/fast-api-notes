8c8889be8bc4b2aeb0fb56d4cef5caa5c39f78a4
Mateusz Kowalczyk <mkowalczyk013@gmail.com>
2024-08-22 23:17:14 +0200

    PostgreSQL db

--- project-03/app/core/database.py
+++ project-03/app/core/database.py
@@ -1,13 +1,29 @@
+import os
 from typing import Annotated, Any, Generator
 
+from dotenv import find_dotenv, load_dotenv
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
 from sqlalchemy.orm.session import Session
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+assert load_dotenv(
+    find_dotenv()
+), "Could not load the environment variables. Is the .env file missing?"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+POSTGRES_USER = os.getenv("POSTGRES_USER", default="postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", default="password")
+POSTGRES_DB = os.getenv("POSTGRES_DB", default="todo-app-db")
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" # SQLite.
+SQLALCHEMY_DATABASE_URL = (  # PostgreSQL.
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost/{POSTGRES_DB}"
+)
+
+# SQLite.
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# PostgreSQL.
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 
---
+++ project-03/db/todo-app-tables.sql
@@ -0,0 +1,26 @@
+DROP      TABLE IF EXISTS users;
+
+CREATE    TABLE users (
+          id SERIAL,
+          email VARCHAR(200) DEFAULT NULL,
+          username VARCHAR(45) DEFAULT NULL,
+          first_name VARCHAR(45) DEFAULT NULL,
+          last_name VARCHAR(45) DEFAULT NULL,
+          hashed_password BYTEA DEFAULT NULL,
+          is_active BOOLEAN DEFAULT NULL,
+          role VARCHAR(45) DEFAULT NULL,
+          PRIMARY KEY (id)
+          );
+
+DROP      TABLE IF EXISTS todos;
+
+CREATE    TABLE todos (
+          id SERIAL,
+          title VARCHAR(200) DEFAULT NULL,
+          description VARCHAR(200) DEFAULT NULL,
+          priority INTEGER DEFAULT NULL,
+          complete BOOLEAN DEFAULT NULL,
+          owner_id INTEGER DEFAULT NULL,
+          PRIMARY KEY (id),
+          FOREIGN KEY (owner_id) REFERENCES users (id)
+          );
